b5ca7a4438b61ad5fbb7336cfb0a14865e8aba1f
Jotapcinfo <prowlerurbanhc@gmail.com>
2022-01-20 18:10:00 -0300

    primeiro commit

---
+++ apiescola/src/main/java/br/com/jotapcinfo/apiescola/ApiescolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jotapcinfo.apiescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiescolaApplication.class, args);
+	}
+
+}
---
+++ apiescola/src/test/java/br/com/jotapcinfo/apiescola/ApiescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jotapcinfo.apiescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
